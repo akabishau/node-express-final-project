ce4fd9307aab16cddc8cdfb7f31e71f18dcb8112
Aleksey Kabishau <aleksey.kabishau@icloud.com>
2023-05-29 14:15:07 -0400

    connect to mongo db

--- app.js
+++ app.js
@@ -18,11 +18,12 @@ app.get('/', (req, res) => {
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
+// server and db connection
 const port = process.env.PORT || 3000
-
+const connectDB = require('./db/connect')
 const start = async () => {
   try {
-    //await connectDB(process.env.MONGO_URI)
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
