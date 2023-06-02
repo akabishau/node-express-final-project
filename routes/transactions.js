59ce7ccbecf0a88292f8fbc24d7a73c8c0351357
Aleksey Kabishau <aleksey.kabishau@icloud.com>
2023-06-02 14:03:00 -0400

    complete the CRUD operations for transactions

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-const { createTransaction } = require('../controllers/transactions')
+const {
+    createTransaction,
+    getAllTransactions,
+    getSingleTransaction,
+    updateTransaction,
+    deleteTransaction
+} = require('../controllers/transactions')
 
-router.route('/').post(createTransaction)
+router.route('/').get(getAllTransactions).post(createTransaction)
+router.route('/:id').get(getSingleTransaction).patch(updateTransaction).delete(deleteTransaction)
 
 module.exports = router
