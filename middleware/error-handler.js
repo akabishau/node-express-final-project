75c973ae9fa6b24606fb412aeb6b8d026001b951
Aleksey Kabishau <aleksey.kabishau@icloud.com>
2023-06-02 14:27:05 -0400

    add error handling comments

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,13 @@
 const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
+
+
 const errorHandlerMiddleware = (err, req, res, next) => {
+  // tutorial 8:50:00
+  // TODO: - handle mongoose validation errors
+  // TODO: - handle mongoose duplicate error
+  // TODO: - handle mongoose not found error
+
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
   }
